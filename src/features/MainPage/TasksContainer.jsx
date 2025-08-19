719291023a62a3596ea3b97b8321455f51d9fa0a
Davitamane <datokhizanishvili005@gmail.com>
2025-08-19 21:17:12 +0400

    Another backup cause im scared

--- src/features/MainPage/TasksContainer.jsx
+++ src/features/MainPage/TasksContainer.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getStatuses } from "../../services/apiQuery";
+import { getStatuses, getTasks } from "../../services/apiQuery";
 import TasksInnerContainer from "./TasksInnerContainer";
 
 function TasksContainer() {
@@ -8,11 +8,20 @@ function TasksContainer() {
     queryFn: getStatuses,
   });
 
+  const tasksQuery = useQuery({
+    queryKey: ["tasks"],
+    queryFn: getTasks,
+  });
+
   if (statusQuery.status === "loading") return null;
+  if (tasksQuery.status === "loading") return null;
 
-  // const tasksByStatus = sta
-
-  // statusQuery.data?.forEach((status) => console.log(status));
+  const tasksByStatus = tasksQuery.data?.reduce((acc, task) => {
+    if (!acc[task.status.id]) acc[task.status.id] = [];
+    acc[task.status.id].push(task);
+    return acc;
+  }, {});
+  console.log(tasksByStatus);
 
   return (
     <div className="grid grid-cols-4 gap-6 mt-8 max-w-screen">
